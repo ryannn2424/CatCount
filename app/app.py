f7e25d69e34152629c6e049cefacabbe2b061dab
ryanray <ryanray25@hotmail.com>
2024-08-07 18:08:39 -0700

    Added CORS support

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 import logging
 import os
 
@@ -6,6 +7,7 @@ import os
 def create_app():
 
     app = Flask(__name__)
+    CORS(app)
     logger = logging.getLogger(__name__)
     logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 
